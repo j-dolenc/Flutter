dff3c0de7a4aa1341e0f6577df7d46c47147ef2c
j-dolenc <jurij1.dolenc@gmail.com>
2023-01-28 10:16:00 +0100

    New object class and transactions

---
+++ expense_planner/lib/transaction.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  //we want to use this as an object not a widget
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
+  });
+}
