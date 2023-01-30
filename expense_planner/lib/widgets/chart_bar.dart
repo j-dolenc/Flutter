e6ab5b673b79ef4e3386bdf0af6b8d88eaa20e89
j-dolenc <jurij1.dolenc@gmail.com>
2023-01-30 16:56:28 +0100

    dynamically set heights

--- expense_planner/lib/widgets/chart_bar.dart
+++ expense_planner/lib/widgets/chart_bar.dart
@@ -7,46 +7,53 @@ class ChartBar extends StatelessWidget {
   ChartBar({this.label, this.spendingAmount, this.spendingPercent});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text('€${spendingAmount.toStringAsFixed(0)}'),
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: [
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text('€${spendingAmount.toStringAsFixed(0)}'),
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1.0),
-                  color: Color.fromARGB(255, 218, 218, 218),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: spendingPercent,
-                child: Container(
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: Color.fromARGB(255, 218, 218, 218),
                     borderRadius: BorderRadius.circular(20),
                   ),
                 ),
-              )
-            ],
+                FractionallySizedBox(
+                  heightFactor: spendingPercent,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(label),
-      ],
-    );
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            child: FittedBox(
+              child: Text(label),
+            ),
+            height: constraints.maxHeight * 0.15,
+          ),
+        ],
+      );
+    });
   }
 }
