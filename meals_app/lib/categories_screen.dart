fe34e175fb51d81a9351cddba1db80e32b2b9e8a
j-dolenc <jurij1.dolenc@gmail.com>
2023-02-04 10:51:05 +0100

    First new screen categories

---
+++ meals_app/lib/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'dummy_data.dart';
+import 'category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Meals"),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (cat) => CategoryItem(cat.title, cat.color),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
